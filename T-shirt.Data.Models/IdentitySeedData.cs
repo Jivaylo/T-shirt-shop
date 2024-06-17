3d87ffca739771a714419e5196d6a3312c8580e8
Ivaylo <ivaylo06@abv.bg>
2024-06-17 15:50:58 +0300

    add one controller and 2 views

--- T-shirt.Data.Models/IdentitySeedData.cs
+++ T-shirt.Data.Models/IdentitySeedData.cs
@@ -7,6 +7,8 @@ namespace T_shirt.Data.Models.Models
 {
     public static class IdentitySeedData
     {
+        private const string adminRole = "Admin";
+
         private const string adminUser = "Admin";
         private const string adminPassword = "Secret123$";
         public static async void EnsurePopulatedIdentity(this IApplicationBuilder app)
@@ -16,7 +18,7 @@ namespace T_shirt.Data.Models.Models
             .CreateScope().ServiceProvider
             .GetRequiredService<RoleManager<IdentityRole>>();
 
-            await roleManager.CreateAsync(new IdentityRole("administrator"));
+            await roleManager.CreateAsync(new IdentityRole("Admin"));
 
 
             StoreDbContext context = app.ApplicationServices
@@ -39,9 +41,9 @@ namespace T_shirt.Data.Models.Models
                 IdentityResult result = await userManager.CreateAsync(user, adminPassword);
             }
 
-            if (!await userManager.IsInRoleAsync(user, "administrator"))
+            if (!await userManager.IsInRoleAsync(user, "Admin"))
             {
-                var rolesAddResult = await userManager.AddToRoleAsync(user, "administrator");
+                var rolesAddResult = await userManager.AddToRoleAsync(user, "Admin");
 
                 var roless = await userManager.GetRolesAsync(user);
             }
---
+++ T-shirt_shop/Controllers/ProductController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using T_shirt.Data.Models;
+using T_shirt.Data.Models.Models;
+using T_shirt.Data.Models.ViewModels;
+
+namespace T_shirt_shop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IStoreRepository _storeRepository;
+
+        public ProductController(IStoreRepository storeRepository) 
+        {
+            _storeRepository = storeRepository;
+        }
+
+        [Authorize(Roles = "Admin"), HttpPost]
+        public async Task<IActionResult> Delete(long productId)
+        {
+            Product? product = await _storeRepository.Products.FirstOrDefaultAsync(x => x.ProductID == productId);
+            if (product == null)
+                return NotFound();
+
+            _storeRepository.DeleteProduct(product);
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [Authorize(Roles = "Admin"), HttpGet]
+        public async Task<IActionResult> Edit(long productId)
+        {
+            Product? product = await _storeRepository.Products.FirstOrDefaultAsync(x => x.ProductID == productId);
+            if (product == null)
+                return NotFound();
+
+            return View(new EditProductViewModel
+            {
+                ProductId = productId,
+                Name = product.Name,
+                Description = product.Description,
+                Price = product.Price
+            });
+        }
+
+        [Authorize(Roles = "Admin"), HttpPost]
+        public async Task<IActionResult> Edit(EditProductViewModel editProductViewModel)
+        {
+            Product? product = await _storeRepository.Products.FirstOrDefaultAsync(x => x.ProductID == editProductViewModel.ProductId);
+            if (product == null)
+                return NotFound();
+
+            product.Name = editProductViewModel.Name;
+            product.Description = editProductViewModel.Description;
+            product.Price = editProductViewModel.Price;
+            _storeRepository.SaveProduct(product);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
